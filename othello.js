17f0e777a95abec404a4f8a6bcf041eed9383dc7
Deep Toaster <ti.42.plus@gmail.com>
2017-01-08 01:57:26 -0800

    fixed styling and simplified repo

---
+++ othello.js
@@ -0,0 +1,96 @@
+function Othello(presenter, readonly) {
+  const dirs = [-11, -10, -9, -1, 1, 9, 10, 11];
+  var player;
+  var states = [];
+  var self = this;
+
+  const cycles = [
+    'green',
+    'yellow',
+    'red'
+  ]
+
+  this.pass = function() {
+    states.push(states[states.length - 1]);
+    player = 1 - player;
+    presenter.setPlayer(player + 1);
+  }
+
+  this.place = function(x, y) {
+    var i = y * 10 + x;
+    var state = states[states.length - 1].split('');
+    var valid = false;
+
+    for (var j = 8; j < 78; j += 10) {
+      state.splice(j, 0, '', '');
+    }
+
+    if (state[i] != '0') {
+      return false;
+    }
+
+    for (var j = 0; j < dirs.length; j++) {
+      var k = i + dirs[j];
+
+      if (state[k] != 2 - player) {
+        continue;
+      }
+
+      while (parseInt(state[k])) {
+        if (state[k] == player + 1) {
+          valid = true;
+
+          do {
+            k -= dirs[j];
+            state[k] = player + 1;
+          } while (i != k);
+
+          break;
+        }
+
+        k += dirs[j];
+      }
+    }
+
+    if (!valid) {
+      return false;
+    }
+
+    states.push(state.join(''));
+    player = 1 - player;
+    presenter.setGrid(states[states.length - 1]);
+    presenter.updateScores();
+    presenter.setPlayer(player + 1);
+    return true;
+  };
+
+  this.reset = function() {
+    presenter.model = self;
+    presenter.createGrid(readonly);
+    states[0] = '00000000'
+              + '00000000'
+              + '00000000'
+              + '00021000'
+              + '00012000'
+              + '00000000'
+              + '00000000'
+              + '00000000';
+    player = 0;
+    presenter.setGrid(states[0]);
+    presenter.updateScores();
+    presenter.setPlayer(1);
+  };
+
+  this.undo = function() {
+    if (states.length < 2) {
+      return false;
+    }
+
+    states.pop();
+    player = 1 - player;
+    presenter.setGrid(states[states.length - 1]);
+    presenter.updateScores();
+    presenter.setPlayer(player + 1);
+    return true;
+  }
+}
---
+++ othellopresenter.js
@@ -0,0 +1,92 @@
+function OthelloPresenter(target) {
+  var self = this;
+  var $grid;
+  var $hud;
+
+  function getCell(x, y) {
+    return $grid.children().eq(y).children().eq(x);
+  }
+
+  this.countCells = function(color) {
+    var count = 0;
+
+    for (var y = 0; y < 8; y++) {
+      for (var x = 0; x < 8; x++) {
+        count += self.getCell(x, y) == color;
+      }
+    }
+
+    return count;
+  }
+
+  this.createGrid = function(readonly) {
+    var grid = '<div class="othello">';
+
+    for (var y = 0; y < 8; y++) {
+      grid += '<div class="othello-row">';
+
+      for (var x = 0; x < 8; x++) {
+        grid += '<div class="othello-cell" />';
+      }
+
+      grid += '</div>';
+    }
+
+    grid += '<div class="othello-hud">'
+        + '<div>Black <span class="othello-score" /></div>'
+        + '<div><span class="othello-score" /> White</div></div></div>';
+    $grid = $(grid).appendTo($(target));
+
+    if (!readonly) {
+      $grid.on('click', '.othello-cell', function() {
+        self.model.place($(this).index(), $(this).parent().index());
+      });
+    }
+  }
+
+  this.getCell = function(x, y) {
+    var cell = getCell(x, y);
+
+    if (cell.hasClass('othello-color1')) {
+      return 1;
+    }
+
+    if (cell.hasClass('othello-color2')) {
+      return 2;
+    }
+
+    return 0;
+  }
+
+  this.setCell = function(x, y, color) {
+    var cell = getCell(x, y).removeClass('othello-color1 othello-color2');
+
+    color = parseInt(color);
+
+    if (color) {
+      cell.addClass('othello-color' + color);
+    }
+  }
+
+  this.setGrid = function(colors) {
+    var i = 0;
+
+    for (var y = 0; y < 8; y++) {
+      for (var x = 0; x < 8; x++) {
+        self.setCell(x, y, colors[i++]);
+      }
+    }
+  }
+
+  this.setPlayer = function(color) {
+    $grid.find('.othello-hud > div').removeClass('active').eq(color - 1)
+        .addClass('active');
+  }
+
+  this.updateScores = function() {
+    var scores = $grid.find('.othello-hud span');
+
+    scores.eq(0).text(self.countCells(1));
+    scores.eq(1).text(self.countCells(2));
+  }
+}
